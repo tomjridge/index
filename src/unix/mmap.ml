83763664a97a538a11a4f1b895f4c1f00b8d1898
Tom Ridge <tom.j.ridge@googlemail.com>
2021-08-26 10:06:10 +0100

    +

--- src/unix/mmap.ml
+++ src/unix/mmap.ml
@@ -1,4 +1,4 @@
 include Mmap_private.Make_2(struct 
     let int_size_is_geq_63 = (Sys.int_size >= 63)
-    let mmap_increment_size = 100_000_000 (* 100M *)
+    let mmap_increment_size = 4_294967296 (* 4GB *)
   end)
