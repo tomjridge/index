0caf1804ab0a13d193d02d0e742ce5c43a752e8a
Tom Ridge <tom.j.ridge@googlemail.com>
2021-09-28 17:13:52 +0100

    Add oracle checking

--- src/index_lite.ml
+++ src/index_lite.ml
@@ -19,7 +19,10 @@
 
 (** Version of index using SQLite for the backend data store. *)
 
-module Kv = Kv_lite.Btree_impl (* Kyoto_impl *)
+(* module Kv = Kv_lite.Kv_hash_impl (\* Kyoto_impl *\) *)
+module Kv' = Kv_lite.Btree_impl (* Kyoto_impl *)
+module Kv = Kv_lite.Hashtbl_impl.Wrap(Kv')
+
 module Thread0 = Thread
 
 include Index_intf
